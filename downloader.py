4691620d5fc0ea7c0642bccd8601d12f0109a117
Tomas Ivan <dev.tomas.ivan@gmail.com>
2018-05-07 10:07:11 +0200

    code cleanup

--- downloader.py
+++ downloader.py
@@ -2,18 +2,20 @@ import json
 import os
 from pprint import pprint
 
+# load cards.json made by casper.js
 with open('cards.json') as f:
     data = json.load(f)
 
-# os.system("mkdir -p \"{0}\"".format("cards/" + data[0]["faction"] + "/" + data[0]["type"]))
-# os.system("wget -O \"{0}\" \"{1}\"".format("cards/" + data[0]["faction"] + "/" + data[0]["type"] + "/" + data[0]["name"] + ".png", data[0]["image"]))
-
+# iterate through json object and load card objects
 for card in data:
     pprint(card["name"])
-    # wget.download(card["image"])
-    # os.system("wget -O \"{0}\" \"{1}\"".format(card["name"] + ".png", card["image"]))
-    # urllib.request.urlretrieve (card["image"], card["name"] + ".png")
 
-    os.system("mkdir -p \"{0}\"".format("cards/" + card["faction"] + "/" + card["type"]))
-    os.system("wget -O \"{0}\" \"{1}\"".format("cards/" + card["faction"] + "/" + card["type"] + "/" + card["name"] + ".png", card["image"]))
+    directory = "cards/" + card["faction"] + "/" + card["type"]
+    filename = card["name"] + ".png"
+
+    # create directory if doesn't exist yet
+    os.system("mkdir -p \"{0}\"".format(directory))
+
+    # download and save image
+    os.system("wget -O \"{0}\" \"{1}\"".format(directory + "/" + filename, card["image"]))
 pass
